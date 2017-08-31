a8fffde287367878f63de413c1b0c3581026c68e
gaCodeHome <1071854678@qq.com>
2017-08-31 15:35:13 +0800

    第一次

---
+++ DownloadTool/DownloadTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadTool
+//
+//  Created by new on 17/8/31.
+//  Copyright © 2017年 magicAo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownloadTool/DownloadTool/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DownloadTool
+//
+//  Created by new on 17/8/31.
+//  Copyright © 2017年 magicAo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
